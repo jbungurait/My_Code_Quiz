e9c21c7176b2c3d1e2629ef7b4d19343a4bfcd45
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-01-22 16:14:36 -0800

    Added timer

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+var quizInterface = document.getElementsByClassName('interface');
+var title = document.getElementById('quizTitle');
+var countdownTimer = document.getElementById('quizTimer');
+var startButton = document.getElementById('start');
+
+function countdown() {
+    var timeLeft = 10; 
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft >1) {
+            countdownTimer.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            countdownTimer.textContent = timeLeft + 'second remaing';
+            timeLeft--;
+        } else {
+            countdownTimer.textContent = 'Out of time!';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+} 
+
+startButton.addEventListener("click", function() {
+    countdown();
+    
+
+})
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # My_Code_Quiz
+
+## User Story
+
+As a coding boot camp student 
+I want to take a timed quiz on JavaScript fundamentals that stores high scores
+So that I can gauge my progress compared to my peers
+
+## Aceptance Criteria 
+
+Given I am taking a code quiz
+When I click the start button
+Then a timer starts and I am presented with a question
+When I answer a question 
+Then I am presented with another question
+When I answer a question incorrectly 
+Then time is subtracted from the clock
+When all questions are answered or the timer reaches 0
+Then the game is over 
+When the game is over 
+Then I can save my initials and my score
+
+
