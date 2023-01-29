8a996062f76acad1cf52e2ae94a66e01aba8ffa2
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-01-29 11:12:58 -0800

    Added questions and answers

--- Assets/script.js
+++ Assets/script.js
@@ -7,14 +7,40 @@ var questions = document.getElementById('Questions');
 var dispQuestion = document.getElementById('dispQuestions');
 
 countdownTimer.textContent = "Click Start to begin.";
+var penalty = 10;
 
 // create questions variables
+var quizQuestions = [
+    {   
+        question: "What function do you use to log messages to the console?",
+        choice: ["window.log", "message.window", "console.log", "error.log",],
+        answer: "console.log",
+    },
+    {
+        question: "What is the correct syntax for declairing a function?",
+        choice: ["function []", "function = ()", "funtion{}", "function()",],
+        answer: "function()",
+    },
+    {
+        question: "What is the index of 'C' in the following array: ['A', 'B', 'C', 'D']?",
+        choice: ["2", "1++", "0", "3",],
+        answer: "2",
+    },
+    {
+        question: "How do you assign a variable to an Id declaired in HMTL?",
+        choice: ["get.element.id()", "getId()", "findId()", "getElementById()",],
+        answer: "getElementById()",
+    },
+    {
+        question: "Which of the following are not array methods?",
+        choice: [".push()", ".slice()", ".add()", ".replace()",],
+        answer: ".add()",
+    },
 
-// create answers array
-
+]
 
 function countdown() {
-    var timeLeft = 10; 
+    var timeLeft = 100; 
 
     var timeInterval = setInterval(function() {
         if (timeLeft >1) {
@@ -31,7 +57,10 @@ function countdown() {
 } 
 
 function populateQuestions() {
-    dispQuestion.textContent = 
+    for (var i = 0; i < quizQuestions.length; i++)
+    {
+     dispQuestion.textContent = quizQuestions[quizQuestions].question;
+    }
 }
 
 startButton.addEventListener("click", function() {
